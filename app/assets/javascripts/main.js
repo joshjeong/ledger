d42e65b239236eb05e39516b7cc2e1c89b3208f4
Josh Jeong <jcjeong@gmail.com>
2014-08-26 19:40:30 -0700

    Ajaxified add feature

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -20,15 +20,37 @@ var Controller = function(view){
   self = this
 
   this.bindEvents = function(){
-    this.addFormListener();
+    this.toggleListener();
+    this.addItemListener();
   }
 
-  this.addFormListener = function(){
-    $('#menu-button').on('click', this.showForm)
+  this.toggleListener = function(){
+    $('#add-btn').on('click', this.showForm)
+  }
+
+  this.addItemListener = function (){
+    $('#log-btn').on('click', function(e){
+      e.preventDefault();
+      self.addItem()
+    })
   }
 
   this.showForm = function(){
     v.toggleForm();
+  }
+
+  this.addItem = function(){
+    var newItem = $('#new_ledger').serialize();
+    var url = $('#log-btn').closest('form').attr('action')
+    var user_id = url.match(/\d/)[0]
+    $.ajax({
+      url: "/add_item",
+      type: "POST",
+      data: newItem + "&user_id=" + user_id
+    }).done(function(response) {
+      $('.ledgers').prepend(response);
+      // $('#new_ledger').find($(".input-field")).val("");
+    })
   }
 
 
--- app/controllers/ledgers_controller.rb
+++ app/controllers/ledgers_controller.rb
@@ -1,18 +1,16 @@
 class LedgersController < ApplicationController
+  skip_before_filter  :verify_authenticity_token
+
   def index
     @user = User.find(params[:user_id])
     @ledgers = @user.ledgers
     @ledger = Ledger.new
   end
 
-  def new
-    @ledger = Ledger.new
-  end
 
   def create
     params.permit!
     @user = User.where('id = ?', params[:user_id])
-    binding.pry
     @user.first.ledgers.create(params[:ledger])
   end
 
@@ -20,6 +18,13 @@ class LedgersController < ApplicationController
     user_id = params[:user_id]
     @ledger = Ledger.find(params[:id])
     @ledgers = Ledger.where('user_id = ? AND name = ?', user_id, @ledger.name)
+  end
+
+  def add_item
+    params.permit!
+    @user = User.where('id = ?', params[:user_id])
+    @ledger = @user.first.ledgers.create(params[:ledger])
+    render :new
   end
 
 end
